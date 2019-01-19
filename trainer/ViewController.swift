aa57811829f4e645902268868f257726db043688
Vladislav Matus <matusvla@fjfi.cvut.cz>
2019-01-19 22:12:44 +0100

    Initial Commit

---
+++ trainer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  trainer
+//
+//  Created by Vladislav Matus on 19/01/2019.
+//  Copyright © 2019 Vladislav Matus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
